a11b51ab34d756bfaf79d8d79c0afa807a0e7789
Amierul Hakeem <amierulhakeem02@gmail.com>
2024-02-16 00:47:56 +0000

    Merging latest bluetooth to main and fixed conflicts

--- main.cpp
+++ main.cpp
@@ -6,36 +6,70 @@
 #include "QEI.h"
 
 
-Serial pc(USBTX, USBRX, 9600);
-Bluetooth bt(PA_11, PA_12); 
+PwmOut LED(LED_PIN);                    // Debug LED set
+Serial pc(USBTX, USBRX, 9600);          // set up serial comm with pc
+Bluetooth bt(BT_TX_PIN, BT_RX_PIN);
+Timer global_timer;                     // set up global program timer
+
+int main_loop_counter = 0;      // just for fun (not important)
+int last_loop_time_us = 0;      // stores the previous loop time
+
+Motor motor_left(MOTORL_PWM_PIN, MOTORL_DIRECTION_PIN, MOTORL_BIPOLAR_PIN);
+Motor motor_right(MOTORR_PWM_PIN , MOTORR_DIRECTION_PIN, MOTORR_BIPOLAR_PIN);
+QEI encoder_left(MOTORL_CHA_PIN, MOTORL_CHB_PIN, NC, 256, QEI::X4_ENCODING);
+QEI encoder_right(MOTORR_CHA_PIN, MOTORR_CHB_PIN, NC, 256, QEI::X4_ENCODING);
 
 
 int main()
 {
-    Motor motor_left(MOTOR_L_PWM_PIN, MOTOR_L_DIRECTION_PIN, MOTOR_L_BIPOLAR_PIN);
-    Motor motor_right(MOTOR_R_PWM_PIN , MOTOR_R_DIRECTION_PIN, MOTOR_R_BIPOLAR_PIN);
-    QEI encoder_left(MOTOR_L_CHA_PIN, MOTOR_L_CHB_PIN, NC, 256, QEI::X4_ENCODING);
-    QEI encoder_right(MOTOR_R_CHA_PIN, MOTOR_R_CHB_PIN, NC, 256, QEI::X4_ENCODING);
-
     motor_left.set_duty_cycle(0.5);
     motor_right.set_duty_cycle(0.5);
 
-    
-    int counter = 0;
-    
-    while (!bt.writeable()) {};
+    LED.period(0.05);
+    global_timer.start();           // Starts the global program timer
+
+    while(!bt.writeable()) {};      // wait for the bluetooth to be ready
 
     while(1)
     {
+        // Checks for recieved bluetooth commands
         if (bt.data_recieved_complete()) {
-            pc.printf("Received: %s\n", bt.get_data());
+            switch (bt.parse_data())
+            {
+                case bt.uturn:
+                    bt.send_fstring("Making a U-turn");
+                    // U-turn code here
+                    break;
+                case bt.get_run_time:
+                    bt.send_fstring("Time: %.2f s", global_timer.read());
+                    break;
+                case bt.set_value:
+                    bt.send_fstring("Duty Cycle: %f", bt.float_data1);
+                    LED.write(bt.float_data1);
+                    break;
+                case bt.get_encoderL_pulses:
+                    bt.send_fstring("L Enc: %d", main_loop_counter);
+                    break;
+                case bt.get_encoderR_pulses:
+                    bt.send_fstring("R Enc: %d", main_loop_counter);
+                    break;
+                // case others:
+                default:
+                    bt.send_fstring("Received: %s\n", bt.get_data());
+                    break;
+            }
             bt.reset_rx_buffer();
         }
 
-        wait_us(1000000);
-        bt.send_fstring("loop number: %d \n", counter);
-        counter++;
 
+        // simulate other part of code:
+        wait_us(1'000'000); // 1 sec delay
+
+
+        // End of loop
+        pc.printf("Loop Time: %d us / %d \n", global_timer.read_us() - last_loop_time_us, global_timer.read_us());
+        last_loop_time_us = global_timer.read_us();
+        main_loop_counter++;
     }
 }
 
