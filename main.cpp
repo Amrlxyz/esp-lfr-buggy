d7792614f09078e2d5043201a7ede26915cbc076
Amierul Hakeem <amierulhakeem02@gmail.com>
2024-02-29 10:35:30 +0000

    small changes and cleaning the code

--- main.cpp
+++ main.cpp
@@ -11,25 +11,27 @@
 #include "motor_driver_board.h"
 
 
+/* BUGGY STATES TYPE */
 typedef enum
 {
     square_mode,
     straight_test,
     PID_test,
-    inactive,
+    task_test,
+    task_test_inactive,
+    inactive,               // motor driver off
 } Buggy_states;
 
 
-/* Variable Declarations */
+/* GLOBAL VARIBLES DECLARATIONS */
 Buggy_states buggy_state = inactive;        // stores buggy states when performing actions
-volatile bool serial_update = false;
-int main_loop_counter = 0;                  // just for fun (not important)
-int last_loop_time_us = 0;                  // stores the previous loop time
-int loop_time_diff = 0;
+volatile bool pc_serial_update = false;
+volatile bool bt_serial_update = false;
+int ISR_exec_time = 0;
 int square_stages = 0;
 
 
-/* Objects Declarations */
+/* OBJECTS DECLARATIONS */
 DigitalOut LED(LED_PIN);                    // Debug LED set
 Serial pc(USBTX, USBRX, 115200);            // set up serial comm with pc
 Bluetooth bt(BT_TX_PIN, BT_RX_PIN);     
@@ -42,34 +44,17 @@ Motor motor_left(MOTORL_PWM_PIN, MOTORL_DIRECTION_PIN, MOTORL_BIPOLAR_PIN);
 Motor motor_right(MOTORR_PWM_PIN , MOTORR_DIRECTION_PIN, MOTORR_BIPOLAR_PIN);
 Encoder encoder_left(MOTORL_CHA_PIN, MOTORL_CHB_PIN);
 Encoder encoder_right(MOTORR_CHA_PIN, MOTORR_CHB_PIN);
+PID PID_motor_left(PID_M_L_KP, PID_M_L_KI, PID_M_L_KD, PID_M_TAU, PID_M_MIN_OUT, PID_M_MAX_OUT, PID_M_MIN_INT, PID_M_MAX_INT);
+PID PID_motor_right(PID_M_R_KP, PID_M_R_KI, PID_M_R_KD, PID_M_TAU, PID_M_MIN_OUT, PID_M_MAX_OUT, PID_M_MIN_INT, PID_M_MAX_INT);
 VectorProcessor vp;
 
-PID PID_motor_left( 
-    PID_M_L_KP, 
-    PID_M_L_KI,
-    PID_M_L_KD,
-    PID_M_TAU,
-    PID_M_MIN_OUT,
-    PID_M_MAX_OUT,
-    PID_M_MIN_INT,
-    PID_M_MAX_INT
-);
 
-PID PID_motor_right(
-    PID_M_R_KP, 
-    PID_M_R_KI,
-    PID_M_R_KD,
-    PID_M_TAU,
-    PID_M_MIN_OUT,
-    PID_M_MAX_OUT,
-    PID_M_MIN_INT,
-    PID_M_MAX_INT
-);
-
-
+/* ISR FUNCTIONS */
 void control_update_ISR(void)
-{
+{   
+    // Get the current time to measure the execution time
     int curr_time = global_timer.read_us();
+
 
     /* Run all the update functions: */
     encoder_left.update();
@@ -78,12 +63,14 @@ void control_update_ISR(void)
         encoder_left.get_tick_count(), 
         encoder_right.get_tick_count());
 
+
     /* PID Calculations: */
     PID_motor_left.update(vp.get_left_set_speed(), encoder_left.get_filtered_speed());
     PID_motor_right.update(vp.get_right_set_speed(), encoder_right.get_filtered_speed());
 
     // pc.printf("%.4f,%.4f\n", encoder_left.get_speed(), encoder_left.get_filtered_speed());
-    pc.printf("%.4f\n", vp.get_cumulative_angle_deg());
+    // pc.printf("%.4f\n", vp.get_cumulative_angle_deg());
+
 
     /* Apply PID output */
     if (buggy_state == PID_test || buggy_state == square_mode)
@@ -95,45 +82,62 @@ void control_update_ISR(void)
         // pc.printf("R - Meas: %f, Out: %f\n", encoder_right.get_speed(), PID_motor_right.get_output());
     }
 
-    // Measure ISR time - Ignore the confusing names for now
-    loop_time_diff = global_timer.read_us() - curr_time;
+    // Measure control ISR execution time
+    ISR_exec_time = global_timer.read_us() - curr_time;
 }
 
 
 void serial_update_ISR(void)
 {
-    // serial_update = true;
+    pc_serial_update = true;
+    bt_serial_update = true;
 }
 
 
-void stop_reset(void)
+/* OTHER FUNCTIONS */
+void square_stage_end(void)
 {
-    // motor_left.set_duty_cycle(0.0);
-    // motor_right.set_duty_cycle(0.0);
     bt.send_fstring("%d, %.4f, %.4f", square_stages, vp.get_cumulative_angle_deg(), vp.get_distance_travelled());
-    // wait_us(1'000'000);
-    // encoder_left.reset();
-    // encoder_right.reset();
-    // vp.reset_distance_travelled();
 }
 
 
+void reset_everything(void)
+{
+    encoder_left.reset();
+    encoder_right.reset();
+    PID_motor_left.reset();
+    PID_motor_right.reset();
+    vp.reset_all();
+}
+
+
+void stop_motors(void)
+{
+    motor_left.set_duty_cycle(0.0);
+    motor_right.set_duty_cycle(0.0);
+}
+
+
+/* MAIN FUNCTION */
 int main()
 {
     while (!bt.is_ready()) {};          // while bluetooth not ready, loop and do nothing
 
+    driver_board.disable();
     global_timer.start();                                                           // Starts the global program timer
     control_ticker.attach_us(&control_update_ISR, CONTROL_UPDATE_PERIOD_US);        // Starts the control ISR update ticker
     serial_ticker.attach(&serial_update_ISR, SERIAL_UPDATE_PERIOD);                 // Starts the control ISR update ticker
 
-    float right_speed_offset = -0.02;
-    
+    // Straight test variables
+    float straight_right_speed_offset = -0.02;
+    float straight_speed = 0.25;
+
     // Square task variables
     float square_angle_set = 0.0;
     float square_distance_set = 0.0;
     float square_velocity_set = 0.4;
-    float square_turning_left_angle = 98;
-    float square_turning_right_angle = 94;
+    float square_turning_left_angle = 103;  // increase to turn more
+    float square_turning_right_angle = 94;  // decrease to turn more 
     float square_distance = 1.01;
 
     while (1)
@@ -165,7 +169,7 @@ int main()
                                 }
                                 break;
                             case bt.speed:                  // S
-                                right_speed_offset = bt.data1;
+                                straight_right_speed_offset = bt.data1;
                                 break;
                             default:
                                 break;
@@ -175,56 +179,58 @@ int main()
                         switch (bt.exec_type)
                         {
                             case bt.stop:
-                                // stop evrything
-                                motor_left.set_duty_cycle(0.0);
-                                motor_right.set_duty_cycle(0.0);
+                                // EMERGENCY STOP
+                                stop_motors();
                                 buggy_state = inactive;
                                 break;
                             case bt.uturn:
-                                // enable uturn code
-                                buggy_state = PID_test; // temporary to test PID
+                                buggy_state = inactive; // WIP
                                 break;
                             case bt.encoder_test:
-                                motor_left.set_duty_cycle(0.0);
-                                motor_right.set_duty_cycle(0.0);
+                                stop_motors();
                                 bt.set_continous(true);
                                 bt.data_type = bt.ticks_cumulative;
                                 bt.obj_type = bt.motor_both;
+                                buggy_state = task_test;
                                 break;
                             case bt.motor_pwm_test:
-                                motor_left.set_duty_cycle(0.0);
-                                motor_right.set_duty_cycle(0.0);
+                                stop_motors();
                                 bt.set_continous(true);
                                 bt.data_type = bt.pwm_duty;
                                 bt.obj_type = bt.motor_both;
+                                buggy_state = task_test_inactive;
                                 break;
                             case bt.straight_test:
-                                // enable move a in a square code
+                                reset_everything();
                                 buggy_state = straight_test;
                                 break;
                             case bt.square_test:
-                                // enable move a in a square code
-                                encoder_left.reset();
-                                encoder_right.reset();
-                                vp.reset_all();
+                                reset_everything();
                                 buggy_state = square_mode;
                                 break;
                             case bt.PID_test:
-                                // test the pid implementation
-                                vp.reset_distance_travelled();
-                                vp.reset_PID_angle();
-                                PID_motor_left.reset();
-                                PID_motor_right.reset();
+                                reset_everything();
                                 vp.set_set_angle(90);
                                 vp.set_set_velocity(0.0);
                                 buggy_state = PID_test;
                                 break;
                             case bt.toggle_led_test:
                                 LED = !LED;
+                                buggy_state = task_test_inactive;
                                 break;
                             default:
                                 break;
                         }
+
+                        if (buggy_state == inactive || buggy_state == task_test_inactive)
+                        {
+                            driver_board.disable();
+                        }
+                        else
+                        {
+                            driver_board.enable();
+                        }
+
                         break;
                     default:
                         break;
@@ -238,10 +244,6 @@ int main()
         }
         /* ---  END OF BLUETOOTH COMMAND HANDLING  --- */
 
-        float straight_speed = 0.25;
-        float turn_speed = 0.25;
-        float distance_travelling = 0.4;
-        float turning_angle = 89.0;
 
         /* --- START OF BUGGY ACTIONS/STATE LOGIC CODE --- */ 
         switch (buggy_state) 
@@ -254,7 +256,7 @@ int main()
                         vp.set_set_angle(square_angle_set);
                         vp.set_set_velocity(square_velocity_set);
                         square_stages++;
-                        stop_reset();
+                        square_stage_end();
                         break;
                     case 7:
                     case 1:
@@ -270,7 +272,7 @@ int main()
                             vp.set_set_angle(square_angle_set);
                             vp.set_set_velocity(0);
                             square_stages++;
-                            stop_reset();
+                            square_stage_end();
                         }
                         break;
                     case 2:
@@ -283,7 +285,7 @@ int main()
                             vp.set_set_angle(square_angle_set);
                             vp.set_set_velocity(square_velocity_set);
                             square_stages++;
-                            stop_reset();
+                            square_stage_end();
                         }
                         break;
                     case 9:
@@ -295,7 +297,7 @@ int main()
                             vp.set_set_angle(square_angle_set);
                             vp.set_set_velocity(0);
                             square_stages++;
-                            stop_reset();
+                            square_stage_end();
                         }
                         break;
                     case 10:
@@ -307,7 +309,7 @@ int main()
                             vp.set_set_angle(square_angle_set);
                             vp.set_set_velocity(square_velocity_set);
                             square_stages++;
-                            stop_reset();
+                            square_stage_end();
                         }
                         break;
                     case 15:
@@ -317,7 +319,7 @@ int main()
                             motor_right.set_duty_cycle(0.0);
                             square_stages = 0;
                             buggy_state = inactive;
-                            stop_reset();
+                            square_stage_end();
                         }
                         break;
                     default:
@@ -326,18 +328,20 @@ int main()
                 break;
             case straight_test:
                 motor_left.set_duty_cycle(straight_speed);
-                motor_right.set_duty_cycle(straight_speed + right_speed_offset);
+                motor_right.set_duty_cycle(straight_speed + straight_right_speed_offset);
                 if (vp.get_distance_travelled() >= 1)
                 {
-                    stop_reset();
                     buggy_state = inactive;
+                    motor_left.set_duty_cycle(0.0);
+                    motor_right.set_duty_cycle(0.0);
+                    reset_everything();
                 }
                 break;
             case PID_test:
                 if (vp.get_distance_travelled() >= 0.5)
                 {
                     buggy_state = inactive;
-                    stop_reset();
+                    reset_everything();
                 }
                 break;
             default:
@@ -347,7 +351,7 @@ int main()
 
         
         /* --- START OF SERIAL UPDATE CODE --- */
-        if (serial_update)
+        if (bt_serial_update)
         {
             // Handling sending data through BT 
             if (bt.is_continous() || bt.is_send_once())
@@ -410,19 +414,22 @@ int main()
                         bt.send_fstring("Runtime: %f", global_timer.read());
                         break;
                     case bt.loop_time:              // X
-                        bt.send_fstring("ISR: %dus", loop_time_diff);
+                        bt.send_fstring("ISR: %dus", ISR_exec_time);
                         break;
                     case bt.loop_count:             // Y
-                        bt.send_fstring("Loop no: %d", main_loop_counter);
+                        bt.send_fstring("removed feature");
                         break;
                     default:
                         break;
                 }
                 bt.set_send_once(false); 
             }
+            bt_serial_update = false;
+        }
 
+        if (pc_serial_update)
+        {
             // PC Serial Debugging code
-
             pc.printf("\n");
             pc.printf("\n");
 
@@ -444,20 +451,13 @@ int main()
 
             pc.printf("\n");
 
-            pc.printf("Calculation ISR Time: %d us / Global Time: %d us \n", loop_time_diff, global_timer.read_us());
+            pc.printf("Calculation ISR Time: %d us / Global Time: %d us \n", ISR_exec_time, global_timer.read_us());
 
-            serial_update = false;
+            pc_serial_update = false;
         }
         /* ---  END OF SERIAL UPDATE CODE  --- */
 
 
-        /*      ARTIFICIAL DELAY FOR DEBUGGING:    */
-        // wait_us(1'000'00);
-
-
         /*       END OF LOOP      */
-        // loop_time_diff = global_timer.read_us() - last_loop_time_us;
-        last_loop_time_us = global_timer.read_us();
-        main_loop_counter++;
     }
 }
